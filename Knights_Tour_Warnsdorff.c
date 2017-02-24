89824edea35c19e8d9a6636443ab8f3756b04b91
Uddalak Bhaduri <bhaduriu@gmail.com>
2017-02-24 12:04:21 +0530

    Update Knights_Tour_Warnsdorff.c

--- Knights_Tour_Warnsdorff.c
+++ Knights_Tour_Warnsdorff.c
@@ -1,52 +1,58 @@
 /*
-    Implemented in C.
     Implementation of a Knight's tour on a 8x8 chessboard using Warnsdorff heuristics.
+    The knight is placed on the first block of an empty board and, moving according to the rules of chess, must visit each square exactly once.
     To know how Warnsdorff Rule works, refer: https://en.wikipedia.org/wiki/Knight%27s_tour#Warnsdorf.27s_rule
+
     Warnsdorff's algorithm is a very simple heuristic method for solving the knight's tour.
     From this algorithm you can start from any initial position of the knight on the board.
     Warnsdorff’s Rule: Always move to an adjacent, unvisited square with minimal degree.
     This algorithm may also more generally be applied to any graph.
-    Although the Hamiltonian path problem is NP-hard in general, this algorithm solves in linear time.
-*/
 
-/*
     ALGORITHM TO IMPLEMENT WARNSDORFF RULE:
     •   Some definitions:
-        • A position Q is accessible from a position P if P can move to Q by a 
+        • A position Q is accessible from a position P if P can move to Q by a
           single Knight's move, and Q has not yet been visited.
-        • The accessibility of a position P is the number of positions accessible 
+        • The accessibility of a position P is the number of positions accessible
           from P.
     •   Algorithm:
         1. set P to be a random initial position on the board
         2. mark the board at P with the move number "1"
-        3. for each move number from 2 to the number of squares on the 
+        3. for each move number from 2 to the number of squares on the
              board:
           1.     let S be the set of positions accessible from the input position
           2.     set P to be the position in S with minimum accessibility
           3.      mark the board at P with the current move number
-        4. return the marked board — each square will be marked with the 
-             move number on which it is visited.  
+        4. return the marked board — each square will be marked with the
+             move number on which it is visited.
+
+Although the Hamiltonian path problem is NP-hard in general, this algorithm solves in linear time.
+Overall Complexity is linear.
 */
 
-
+// C program for Knight Tour problem using Warnsdorff's Heuristics
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #define N 8
+/* Move pattern on basis of the change of x coordinates and y coordinates respectively */
 static int cx[N]={1,1,2,2,-1,-1,-2,-2};
-static int cy[N]={2,-2,1,-1,2,-2,1,-1};
+static int cy[N]={2,-2,1,-1,2,-2,1,-1};//to maintain the knight's position, i.e. ;
+
+/* function restricts the knight to remain within the 8x8 chessboard */
 int limits(int *a, int x, int y)
 {
     if((x>=0 && y>=0) && (x<N && y<N))
         return 1;
     return 0;
 }
+/* checks whether a square is empty or not */
 int isempty(int *a, int x, int y)
 {
     if((limits(a,x,y)) && (a[y*N+x]<0))
         return 1;
     return 0;
 }
+/* returns the number of empty squares */
 int getcount(int *a, int x, int y)
 {
     int i,count=0;
@@ -57,6 +63,7 @@ int getcount(int *a, int x, int y)
     }
     return count;
 }
+/* calculates the minimum degree(count) of unvisited square among its neighbours and assigns the counter to that square */
 int progress(int *a, int *x, int *y)
 {
     int start,count,i,flag=-1,c,min=(N+1),nx,ny;
@@ -80,6 +87,7 @@ int progress(int *a, int *x, int *y)
     *x=nx;*y=ny;
     return 1;
 }
+/* displays the chessboard with all the legal knight's moves */
 void print(int *a)
 {
     int i,j;
@@ -90,7 +98,9 @@ void print(int *a)
         printf("\n");
     }
 }
-int neighbor(int x, int y, int xx, int yy)
+/* checks its neighbouring sqaures */
+/* If the knight ends on a square that is one knight's move from the beginning square, then tour is closed */
+int neighbour(int x, int y, int xx, int yy)
 {
     int i;
     for(i=0;i<N;++i)
@@ -100,15 +110,16 @@ int neighbor(int x, int y, int xx, int yy)
     }
     return 0;
 }
+/* generates the legal moves using warnsdorff's heuristics */
 int generate()
 {
     int a[N*N];
     int i,x,y,sx,sy;
     for(i=0;i<N*N;++i)
-        a[i]=-1;
+        a[i]=-1;//filling up the chessboard matrix with -1's
     sx=x=rand()%N;
     sy=y=rand()%N;//if you want to start from the first square of the chessboard, then set x<==0 and y<==0
-    a[y*N+x]=1;
+    a[y*N+x]=1;//initial position
     for(i=0;i<N*N-1;++i)
     {
         if(progress(a,&x,&y)==0)
@@ -116,11 +127,12 @@ int generate()
             return 0;
         }
     }
-    if(!neighbor(x,y,sx,sy))
+    if(!neighbour(x,y,sx,sy))
         return 0;
     print(a);
     return 1;
 }
+/* Driver program to test above functions */
 int main()
 {
     srand(time(NULL));
